6406af438aa055adc77f3ccb8471ef6f44af1bce
Harish Manana <harish.manana@stackroute.in>
2021-01-26 07:49:55 +0530

    v-2

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,5 +3,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first project using Java");
+		System.out.println("New line added");
 	}
 }
---
+++ src/NewFile.java
@@ -0,0 +1,9 @@
+
+public class NewFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is new file");
+	}
+
+}
